63e8228743326e0931c053c15c6e3e415420f8bd
MCBHammer <armanaznavour@yahoo.com>
2020-10-05 23:09:44 -0500

    Added Health Slider

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : MonoBehaviour
+{
+    [SerializeField] Slider _healthSlider = null;
+    public int _topHealth = 100;
+    int _health;
+
+    void Start()
+    {
+        _healthSlider.maxValue = _topHealth;
+        _healthSlider.value = _topHealth;
+        _health = _topHealth;
+    }
+
+    void Update()
+    {
+        _healthSlider.value = _health;
+    }
+}
