f813fa6e3d681669b776606dc77b1e932a65faf5
MCBHammer <armanaznavour@yahoo.com>
2020-10-05 21:51:29 -0500

    2B Setup Doc Complete
    Player Movement is Done BB

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FPSInput))]
+[RequireComponent(typeof(FPSMotor))]
+public class PlayerController : MonoBehaviour
+{
+    FPSInput _input = null;
+    FPSMotor _motor = null;
+
+    [SerializeField] float _moveSpeed = .1f;
+    [SerializeField] float _turnSpeed = 6f;
+    [SerializeField] float _jumpStrength = 20f;
+
+    private void Awake()
+    {
+        _input = GetComponent<FPSInput>();
+        _motor = GetComponent<FPSMotor>();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable()
+    {
+        _input.MoveInput += OnMove;
+        _input.RotateInput += OnRotate;
+        _input.JumpInput += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        _input.MoveInput -= OnMove;
+        _input.RotateInput -= OnRotate;
+        _input.JumpInput -= OnJump;
+    }
+
+    void OnMove(Vector3 movement)
+    {
+        _motor.Move(movement * _moveSpeed);
+    }
+
+    void OnRotate(Vector3 rotation)
+    {
+        _motor.Turn(rotation.y * _turnSpeed);
+        _motor.Look(rotation.x * _turnSpeed);
+    }
+
+    void OnJump()
+    {
+        _motor.Jump(_jumpStrength);
+    }
+}
