8d0cf21e0055babb4d1cc28d8a1c73b35c0f9715
MCBHammer <armanaznavour@yahoo.com>
2020-10-25 02:21:10 -0500

    Weapon Raycast

---
+++ Assets/Scripts/FireWeapon.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWeapon : MonoBehaviour
+{
+    [SerializeField] Camera cameraController;
+    [SerializeField] Transform rayOrigin;
+    [SerializeField] float shootDistance = 10f;
+
+    RaycastHit objectHit;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    //fire weapon with a raycast
+    void Shoot()
+    {
+        Vector3 rayDirection = cameraController.transform.forward;
+        Debug.DrawRay(rayOrigin.position, rayDirection * shootDistance, Color.yellow, 1f);
+
+        if(Physics.Raycast(rayOrigin.position, rayDirection, out objectHit, shootDistance))
+        {
+            Debug.Log("You hit the " + objectHit.transform.name);
+        } else
+        {
+            Debug.Log("Miss");
+        }
+    }
+}
