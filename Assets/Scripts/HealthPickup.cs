b51a4c3d979e6d0a137f3cace250e12db6c46ba2
MCBHammer <armanaznavour@yahoo.com>
2020-10-26 04:12:30 -0500

    Health Pickup

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    [SerializeField] GameData _gameData = null;
+    public int _healthRestored = 20;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _gameData._health += _healthRestored;
+            Destroy(this.gameObject);
+        }
+    }
+}
